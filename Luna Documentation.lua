734addde8cf2b20c9a0c8f3e369aa46fbdd9138b
Zepsyy <102182440+ZepsyyCodesLUA@users.noreply.github.com>
2022-03-27 13:59:47 -0500

    Update Luna Documentation.lua

--- Luna Documentation.lua
+++ Luna Documentation.lua
@@ -3,6 +3,34 @@ local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZepsyyCo
 
 --Luna Window--
 local win = WindowTable:Window({
-<string> Title
-<string> gameName
+   <string> Title --Window Title here..
+   <string> gameName -- You Can Also Add Credits Here As Well!
 })
+
+--New Tab--
+
+local tab = win:CreateTab({
+	<string> tabtitle --here will make a new tab button and page listed as the tab title you named it
+})
+
+--New Label--
+tab:Label({
+	<string> LabelText --Here you can use it as if it's a section for for some miscellaneous scripts.
+})
+
+--New Button--
+tab:CreateButton(<string> Text, function()
+	  --Script Here
+end)
+
+--New Toggle--
+tab:Toggle(<string> text, function(bool)
+    
+end)
+
+--New Slider--
+tab:Slider(<string> Text, minvalue, maxvalue, function(args)
+    print(args)
+end)
+
+--Please Join My Discord and DM me for any Problems https://discord.gg/AyX4BVn3fE[This Discord Server Has Not Been Fully created yet.]
