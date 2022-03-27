daaf9fbc6964f918e36d4b7d5629ae0e155bcc4b
Zepsyy <102182440+ZepsyyCodesLUA@users.noreply.github.com>
2022-03-27 13:51:49 -0500

    Update Luna Documentation.lua

--- Luna Documentation.lua
+++ Luna Documentation.lua
@@ -1,1 +1,8 @@
+--Luna Loadstring--
+local Luna = loadstring(game:HttpGet("https://raw.githubusercontent.com/ZepsyyCodesLUA/Luna-UI/main/Luna%20Open%20Source.lua"))() --Adds Loadstring For other modules to catch script.
 
+--Luna Window--
+local win = WindowTable:Window({
+	<string> Title
+	<string> gameName
+})
